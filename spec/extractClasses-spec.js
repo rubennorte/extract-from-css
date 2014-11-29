b1459d703847524798c80cf39d1470721bb3b44a
Rubén Norte <rubennorte@gmail.com>
2014-11-29 01:27:32 +0100

    Initial version 0.1.0

---
+++ spec/extractClasses-spec.js
@@ -0,0 +1,106 @@
+
+var extractClasses = require('..').extractClasses;
+var equalsIgnoreOrderMatcher = require('./helpers/equals-ignore-order-matcher');
+
+describe('extractClasses', function() {
+
+  beforeAll(function() {
+    jasmine.addMatchers(equalsIgnoreOrderMatcher);
+  });
+
+  describe('extract class names from simple rules', function() {
+    it('should extract a class name with alphabetical letters', function() {
+      var classNames = extractClasses('.className { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['className']);
+    });
+
+    it('should several class names with alphabetical letters', function() {
+      var classNames = extractClasses('.className1 { prop: value; } .className2 { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['className1', 'className2']);
+    });
+
+    it('should not repeat class names', function() {
+      var classNames = extractClasses('.className1 { prop: value; } .className1 { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['className1']);
+    });
+
+    it('should extract class names with underscores', function() {
+      var classNames = extractClasses('.class_name { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['class_name']);
+    });
+
+    it('should extract class names with hyphens', function() {
+      var classNames = extractClasses('.class-name { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['class-name']);
+    });
+  });
+
+  describe('extract class names from nested rules', function() {
+    it('should extract class names inside a media query', function() {
+      var classNames = extractClasses('@media (max-width: 500px) { .className { prop: value; } }');
+      expect(classNames).toEqualIgnoreOrder(['className']);
+    });
+    it('should extract class names inside two media queries', function() {
+      var classNames = extractClasses('@media (max-width: 500px) { @media screen { .className { prop: value; } } }');
+      expect(classNames).toEqualIgnoreOrder(['className']);
+    });
+  });
+
+  describe('extract complex class names', function() {
+    it('should extract class names with single escaped characters', function() {
+      var classNames = extractClasses('.hello-\\#-world { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['hello-#-world']);
+    });
+    it('should extract class names with multiple escaped characters', function() {
+      var classNames = extractClasses('.\\0000E9dition { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['édition']);
+    });
+    it('should extract class names from multiple escaped characters with white space', function() {
+      var classNames = extractClasses('.\\E9 dition { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['édition']);
+    });
+  });
+
+  describe('extract class names from complex selectors', function() {
+    it('should extract class names at the beginning of a complex selector', function() {
+      var classNames = extractClasses('.class-name#identifier[attr]:hover:after { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['class-name']);
+    });
+    it('should extract class names at the middle of a complex selector', function() {
+      var classNames = extractClasses('#identifier[attr].class-name:hover:after { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['class-name']);
+    });
+    it('should extract class names at the end of a complex selector', function() {
+      var classNames = extractClasses('#identifier[attr]:hover:after.class-name { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['class-name']);
+    });
+    it('should extract several class names in the same selector', function() {
+      var classNames = extractClasses('.class-name-1.class-name { prop: value; }');
+      expect(classNames).toEqualIgnoreOrder(['class-name-1', 'class-name']);
+    });
+  });
+
+  describe('ignore everthing but selectors', function() {
+    it('should ignore class names in declaration blocks', function() {
+      // .png looks like a class selector
+      var classNames = extractClasses('.className { prop: url("something.png") }');
+      expect(classNames).toEqualIgnoreOrder(['className']);
+    });
+
+    it('should ignore class names in regular comments', function() {
+      var classNames = extractClasses('/* .className {} */ .className1 {}');
+      expect(classNames).toEqualIgnoreOrder(['className1']);
+    });
+
+    it('should ignore class names in multi-line comments', function() {
+      var classNames = extractClasses('/* .className {} \n .className1 {} */ .className2 {}');
+      expect(classNames).toEqualIgnoreOrder(['className2']);
+    });
+
+    it('should ignore class names in comments between selectors', function() {
+      var classNames = extractClasses('.className, /* .className1 */ .className2 {}');
+      expect(classNames).toEqualIgnoreOrder(['className', 'className2']);
+    });
+  });
+
+});
---
+++ spec/helpers/equals-ignore-order-matcher.js
@@ -0,0 +1,42 @@
+
+function inArray(array, el) {
+  for (var i = array.length; i--;) {
+    if (array[i] === el) return true;
+  }
+  return false;
+}
+
+function equalsIgnoreOrder(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (var i = arr1.length; i--; ) {
+    if (!inArray(arr2, arr1[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function compareIgnoreOrder(actual, expected) {
+  var result = {};
+
+  result.pass = equalsIgnoreOrder(actual, expected);
+
+  if (!result.pass) {
+    result.message = 'Expected ' + actual.toString() +
+        ' to be equal (ignoring order) to ' + expected.toString();
+  }
+
+  return result;
+}
+
+var matcher = {
+  toEqualIgnoreOrder: function() {
+    return {
+      compare: compareIgnoreOrder
+    };
+  }
+}
+
+module.exports = matcher;

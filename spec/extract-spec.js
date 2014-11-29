0cba844c1a8743ca6349704b4e5419af9caa4f23
Rubén Norte <rubennorte@gmail.com>
2014-11-29 23:17:53 +0100

    Adds extract function and improves benchmark

---
+++ spec/extract-spec.js
@@ -0,0 +1,34 @@
+
+var extract = require('..');
+
+describe('extract', function() {
+
+  var selector = '#id, .class {}';
+
+  it('should extract class names', function() {
+    var result = extract(['classes'], selector);
+    expect(result).toEqual({classes: ['class']});
+  });
+
+  it('should extract ids', function() {
+    var result = extract(['ids'], selector);
+    expect(result).toEqual({ids: ['id']});
+  });
+
+  it('should extract multiple features', function() {
+    var result = extract(['ids', 'classes'], selector);
+    expect(result).toEqual({ids: ['id'], classes: ['class']});
+  });
+
+  it('should throw an error when extracting unknown features', function() {
+    expect(function() {
+      extract(['ids', 'potatoes'], selector);
+    }).toThrow();
+  });
+
+  it('should return an empty object when extracting no features', function() {
+    var result = extract([], selector);
+    expect(result).toEqual({});
+  });
+
+});
--- test/benchmark.js
+++ test/benchmark.js
@@ -15,18 +15,24 @@ var samples = fs.readdirSync(samplesDir).map(function(sampleFile) {
 });
 
 samples.forEach(function(sample, index) {
+  suite.add('extract ids and classes from sample #' + index, function() {
+    extract(['ids', 'classes'], sample);
+  });
   suite.add('extractClasses from sample #' + index, function() {
     extractClasses(sample);
   });
-});
-samples.forEach(function(sample, index) {
   suite.add('extractIds from sample #' + index, function() {
     extractIds(sample);
   });
 });
 
 suite.on('cycle', function(event) {
-  console.log(String(event.target));
+  var stats = event.target.stats;
+  var showStats = ['mean', 'deviation'];
+  var statsSummary = showStats.map(function(statName) {
+    return statName + '=' + stats[statName].toFixed(4);
+  });
+  console.log(event.target.name + ' (' + statsSummary.join(', ') + ')');
 });
 
 suite.run();
